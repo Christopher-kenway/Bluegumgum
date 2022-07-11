65584b5c2c0c53454fb094270277751f0007380a
Christopher-kenway <maero1eguwe@gmail.com>
2022-07-11 08:06:07 +0100

    new

--- examples/JAVASCRIPT LEARNING/JS08/js/script2.js
+++ examples/JAVASCRIPT LEARNING/JS08/js/script2.js
@@ -1,5 +1,16 @@
-var johnGreeter = {};
-johnGreeter.name = "kenway";
-johnGreeter.sayHi = function () {
-	console.log("hi " + johnGreeter.name);
+<script>
+var car = {
+    startEngine: function () {
+        console.log("Car started");             
+    }        
 }
+  
+var bike = {
+    startEngine: function () {
+        console.log("Bike started");
+    }
+}
+  
+car.startEngine();
+bike.startEngine();
+</script>
